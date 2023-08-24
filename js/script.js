7f6e51888ad0044d96f826f2b0f13b6452e7e2b6
colorodriguezf <colitorodriguez97@gmail.com>
2023-08-24 12:49:00 -0300

    se agrego funcionalidad para poder agregar, tachar/eliminar tarea

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const taskList = document.getElementById('taskList');
+const addBtn = document.getElementById('addBtn');
+
+addBtn.addEventListener('click', addTask);
+taskList.addEventListener('click', toggleTask);
+taskList.addEventListener('click', deleteTask);
+
+function addTask() {
+  const taskInput = document.getElementById('task');
+  const newTaskText = taskInput.value.trim();
+  
+  if (newTaskText !== '') {
+    const newTask = document.createElement('li');
+    
+    const taskParagraph = document.createElement('p');
+    taskParagraph.textContent = newTaskText;
+    newTask.appendChild(taskParagraph);
+    
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Eliminar';
+    deleteBtn.classList.add('deleteBtn');
+    newTask.appendChild(deleteBtn);
+    
+    taskList.appendChild(newTask);
+    taskInput.value = '';
+  }
+}
+
+function toggleTask(event) {
+  if (event.target.tagName === 'P') {
+    event.target.classList.toggle('completed');
+  }
+}
+
+// ... Resto de tu código ...
